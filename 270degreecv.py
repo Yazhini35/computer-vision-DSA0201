3b23cb3ba385f97a1c28dc0af143c2992f7c4768
Yazhini35 <120041954+Yazhini35@users.noreply.github.com>
2023-05-10 08:55:57 +0530

    Add files via upload

---
+++ 270degreecv.py
@@ -0,0 +1,14 @@
+import cv2
+ 
+
+path = "C:/Users/YAZHINI/Downloads/download.jfif"
+ 
+src = cv2.imread(path)
+ 
+window_name = 'Image'
+
+image = cv2.rotate(src, cv2.ROTATE_90_COUNTERCLOCKWISE)
+ 
+# Displaying the image
+cv2.imshow(window_name, image)
+cv2.waitKey(0)
